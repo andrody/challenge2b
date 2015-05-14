487553d6169b8bec92228886222a8a207d142d9a
Bruno Rolim <bruno_rocha2008@hotmail.com>
2015-05-14 08:41:28 -0300

    botei minifase1

--- Ninja Goo/MenuViewController.swift
+++ Ninja Goo/MenuViewController.swift
@@ -16,7 +16,7 @@ class MenuViewController: UIViewController, UIPageViewControllerDataSource {
     
     // Initialize it right away here
     private let contentImages = [["fundoMenu"],
-        ["minifase4", "minifase2","minifase3"]
+        ["minifase1", "minifase2","minifase3"]
     ];
     
     override func viewDidLoad() {
