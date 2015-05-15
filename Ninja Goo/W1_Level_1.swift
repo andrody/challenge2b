56264b92d995ad5fe3f02a2429ff534822b6c12a
andrody <andrewcsz@gmail.com>
2015-05-15 14:40:11 -0300

    merge

--- Ninja Goo/W1_Level_1.swift
+++ Ninja Goo/W1_Level_1.swift
@@ -321,7 +321,7 @@ class W1_Level_1: SKScene, SKPhysicsContactDelegate {
 //                        
 //                        tile.physicsBody = SKPhysicsBody(rectangleOfSize:tile.size)
 //                        tile.physicsBody!.categoryBitMask = ColliderType.Wall.rawValue
-// /Users/brunorolim/Dropbox/Challenge2/Ninja Goo Assets/Maps/zerofase/minifase6.tmx                       tile.physicsBody!.dynamic = false
+//                        tile.physicsBody!.dynamic = false
 //                        tile.physicsBody!.friction = 0
 //
 //                    }
@@ -358,7 +358,7 @@ class W1_Level_1: SKScene, SKPhysicsContactDelegate {
                         //if(rotation != nil) {
                         let radians = ConvertUtilities.degreesToRadians(CGFloat(rotation!))
                         
-                        let rotateAction = SKAction.rotateByAngle(radians, duration: NSTimeInterval(20.0))
+                        let rotateAction = SKAction.rotateByAngle(radians, duration: NSTimeInterval(5.0))
                         
                         let action = SKAction.repeatActionForever(rotateAction)
                         tile.runAction(action)
